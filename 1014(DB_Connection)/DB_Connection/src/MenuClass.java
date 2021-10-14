097c0004982d85d8cb2e6925102434698c7c1b76
peaks26 <88639900+peaks26@users.noreply.github.com>
2021-10-14 14:13:34 +0900

    Add files via upload

---
+++ 1014(DB_Connection)/DB_Connection/src/MenuClass.java
@@ -0,0 +1,9 @@
+public class MenuClass {
+	public static void menu() {
+		System.out.println("1.데이터 모두조회");
+		System.out.println("2.데이터 조건조회");
+		System.out.println("3.데이터 삽입");
+		System.out.println("4.데이터 수정");
+		System.out.println("5.데이터 삭제");
+	}
+}
